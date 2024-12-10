8b4d00c665e80a94c0e84f0c405a26b6c032de95
mendesLet <limendes1305@gmail.com>
2024-12-10 09:23:31 -0300

    feat: Changed to use moo-moo-farm and universe-starter-agent as volume

--- utils/worker.sh
+++ utils/worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run our worker process
-CUDA_VISIBLE_DEVICES= python ./scripts/worker.py --log-dir /tmp/marioKartLogs --env-id Mario-Kart-Discrete-Luigi-Raceway-v0 --job-name worker --remotes 1 >/tmp/marioKartLogs/a3c.w-`hostname`.out 2>&1 &
+CUDA_VISIBLE_DEVICES= python ./scripts/worker.py --log-dir /tmp/marioKartLogs --env-id Mario-Kart-Discrete-Moo-Moo-Farm-v0 --job-name worker --remotes 1 >/tmp/marioKartLogs/a3c.w-`hostname`.out 2>&1 &
 
 # Wait for Xvfb to begin running
 until ps aux | grep -i "[x]vfb" > /dev/null; do
